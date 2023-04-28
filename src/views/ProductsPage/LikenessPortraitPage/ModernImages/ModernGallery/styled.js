ffad44ef8c5fa025177241d9898f7b17aabcf004
MaryGuk <mkosevskaa@gmail.com>
2023-04-28 23:49:38 +0300

    create a new file modern gallery

---
+++ src/views/ProductsPage/LikenessPortraitPage/ModernImages/ModernGallery/styled.js
@@ -0,0 +1,12 @@
+import { Box, styled } from "@mui/material";
+
+export const ModernImagesGallery = styled(Box)`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(170px, 1fr));
+  grid-column-gap: 1px;
+  grid-row-gap: 50px;
+  width: 53%;
+  margin: 40px auto;
+`;
+
+export const ModernImagesItems = styled(Box)``;
--- src/views/ProductsPage/LikenessPortraitPage/ModernImages/styled.js
+++ src/views/ProductsPage/LikenessPortraitPage/ModernImages/styled.js
@@ -22,14 +22,3 @@ export const ModernImagesDescription = styled(Box)`
   text-align: center;
   color: #555555;
 `;
-
-export const ModernImagesGallery = styled(Box)`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(170px, 1fr));
-  grid-column-gap: 1px;
-  grid-row-gap: 50px;
-  width: 53%;
-  margin: 40px auto;
-`;
-
-export const ModernImagesItems = styled(Box)``;
