2a1ee324fe46b51470a95fa3e79a8bdc53451f91
MaryGuk <mkosevskaa@gmail.com>
2023-05-03 01:40:54 +0300

    footer adaptive

--- src/components/Pages/Footer/footer.jsx
+++ src/components/Pages/Footer/footer.jsx
@@ -13,32 +13,30 @@ const Footer = () => {
           <div className="footer-content">
             <h1 className="footer-logo"> kotvangog</h1>
             <div className="footer-contacts">
-              <span className="address">{t("footer.address")}</span>
-              <span className="address-city">{t("footer.address_city")} </span>
-              <span className="address-metro">{t("footer.address_metro")}</span>
-              <span className="address-street">
-                {t("footer.address_street")}
-              </span>
-              <span className="address-street__more">
+              <div className="address">{t("footer.address")}</div>
+              <div className="address-city">{t("footer.address_city")} </div>
+              <div className="address-metro">{t("footer.address_metro")}</div>
+              <div className="address-street">{t("footer.address_street")}</div>
+              <div className="address-street__more">
                 {t("footer.address-street__more")}
-              </span>
-              <span className="phone-number"> +7 (993) 360-07-10 </span>
+              </div>
+              <div className="phone-number"> +7 (993) 360-07-10 </div>
               <div className="location-container">
                 <a href="https://yandex.by/maps/-/CCUzJVxNXC" target="_blank">
-                  <span className="address-location">
+                  <div className="address-location">
                     <img
                       className="location-image"
                       src="/images/location-icon.png"
                       alt=""
                     />
                     {t("footer.location_image")}
-                  </span>
+                  </div>
                 </a>
               </div>
             </div>
           </div>
-        
-            <div className="footer-content__navbar">
+
+          <div className="footer-content__navbar">
             <div className="footer-navbar">
               <NavLink to="/home">{t("footer.footer_navbar1")} </NavLink>
             </div>
@@ -48,17 +46,14 @@ const Footer = () => {
             </div>
 
             <div className="footer-navbar">
-    
               <NavLink to="/reviews">{t("footer.footer_navbar3")}</NavLink>
             </div>
           </div>
-        
-         
 
           <div className="footer-content">
-            <span className="footer-messenger">
+            <div className="footer-messenger">
               {t("footer.footer-messenger")}
-            </span>
+            </div>
             <div className="footer-icons">
               <a href="https://t.me/kot_vangog" target="_blank">
                 <img
