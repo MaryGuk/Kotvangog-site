7d9ca524562d9d87f9326d211870c3f1c221efb3
MaryGuk <mkosevskaa@gmail.com>
2023-05-22 11:44:55 +0300

    oil portrait photo replacement

--- src/views/ProductsPage/OilPortraitPage/OilPortraitSecond/index.jsx
+++ src/views/ProductsPage/OilPortraitPage/OilPortraitSecond/index.jsx
@@ -19,25 +19,25 @@ const OilPortraitSecond = () => {
               <img
                 width="350px"
                 height="auto"
-                src="https://www.dropbox.com/s/j9f38of6c7b0kta/%D0%BC%D0%B0%D1%81%D0%BB%D0%BE%201.jpg?raw=1"
+                src="https://www.dropbox.com/s/53qq4dm1ltqkk9s/1%20%D1%84%D0%BE%D1%82%D0%BE.jpg?raw=1"
                 loading="lazy"
                 decoding="async"
               />
             </OilPortraitSecondContentImages>
             <OilPortraitSecondContentImages>
               <img
-                width="260px"
-                height="auto"
-                src="https://www.dropbox.com/s/op4t2f2dbjirivz/%D0%BC%D0%B0%D1%81%D0%BB%D0%BE%202.jpg?raw=1"
+                width="auto"
+                height="325px"
+                src="https://www.dropbox.com/s/9hczbr3fxia72g0/2%20%D1%84%D0%BE%D1%82%D0%BE.jpg?raw=1"
                 loading="lazy"
                 decoding="async"
               />
             </OilPortraitSecondContentImages>
             <OilPortraitSecondContentImages>
               <img
-                width="260px"
-                height="auto"
-                src="https://www.dropbox.com/s/u6hbcynxb56s89r/%D0%BC%D0%B0%D1%81%D0%BB%D0%BE%203.jpg?raw=1"
+                width="auto"
+                height="326px"
+                src="https://www.dropbox.com/s/45jazoryhehrv7c/3%20%D1%84%D0%BE%D1%82%D0%BE.jpg?raw=1"
                 loading="lazy"
                 decoding="async"
               />
