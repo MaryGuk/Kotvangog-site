4be86b8f3d50626e3f7c1ab5de5e777f6579febc
MaryGuk <mkosevskaa@gmail.com>
2023-05-25 10:31:55 +0300

    add ellipse container advantage page mobile first

--- src/components/Pages/AdvantagesPage/AdvantageItem.jsx
+++ src/components/Pages/AdvantagesPage/AdvantageItem.jsx
@@ -1,11 +1,17 @@
+import { isMobile } from "react-device-detect";
+
 const AdvantageItem = ({ imageSrc, description }) => {
   return (
     <div className="advantage-item__wrapper">
-      <div className="advantage-item__container">
-        <img className="advantages-img" src={imageSrc} alt="" />
-      </div>
+      {isMobile ? (
+        <div className="ellipse-container">
+          <div className="advantage-item__container">
+            <img className="advantages-img" src={imageSrc} alt="" />
+          </div>
 
-      <div className="advantages-text">{description}</div>
+          <div className="advantages-text">{description}</div>
+        </div>
+      ) : null}
     </div>
   );
 };
