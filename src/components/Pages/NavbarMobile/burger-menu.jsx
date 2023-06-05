09a724f096766ed198921e7e25cf350b933d0e0e
viacheslav <viacheslav.g@5ly.co>
2023-06-05 04:09:18 +0300

    add close on click away

--- src/components/Pages/NavbarMobile/burger-menu.jsx
+++ src/components/Pages/NavbarMobile/burger-menu.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import "./burger-menu.css";
 import { NavLink } from "react-router-dom";
+import {ClickAwayHandler, NavbarParent} from "./styled";
 
 function BurgerMenu() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleMenuClick = () => {
-    setIsOpen(!isOpen);
-  };
+    const handleMenuClick = () => {
+        setIsOpen(!isOpen);
+    };
 
-  const handleCloseNavbar = () => {
-    setIsOpen(false);
-  };
+    const handleCloseNavbar = () => {
+        setIsOpen(false);
+    };
 
   return (
     <div className="burger-menu">
@@ -28,55 +29,58 @@ function BurgerMenu() {
         <div className="burger-menu__text"> KOTVANGOG </div>
       </div>
       {isOpen && (
-        <div className="burger-menu__content">
-          <ul>
-            <li>
-              <NavLink
-                style={{ textDecoration: "none", color: "white" }}
-                to="/home"
-                onClick={handleCloseNavbar}
-              >
-                Главная
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                onClick={handleCloseNavbar}
-                style={{ textDecoration: "none", color: "white" }}
-                to="/oil-portrait"
-              >
-                Портрет маслом
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                onClick={handleCloseNavbar}
-                style={{ textDecoration: "none", color: "white" }}
-                to="/products"
-              >
-                Арт стили
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                onClick={handleCloseNavbar}
-                style={{ textDecoration: "none", color: "white" }}
-                to="/likeness-portrait"
-              >
-                Портрет в образе
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                onClick={handleCloseNavbar}
-                style={{ textDecoration: "none", color: "white" }}
-                to="/reviews"
-              >
-                Отзывы
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+          <NavbarParent>
+            <div className="burger-menu__content">
+              <ul>
+                  <li>
+                      <NavLink
+                          style={{ textDecoration: "none", color: "white" }}
+                          to="/home"
+                          onClick={handleCloseNavbar}
+                      >
+                          Главная
+                      </NavLink>
+                  </li>
+                  <li>
+                      <NavLink
+                          style={{ textDecoration: "none", color: "white" }}
+                          to="/oil-portrait"
+                          onClick={handleCloseNavbar}
+                      >
+                          Портрет маслом
+                      </NavLink>
+                  </li>
+                  <li>
+                      <NavLink
+                          style={{ textDecoration: "none", color: "white" }}
+                          to="/products"
+                          onClick={handleCloseNavbar}
+                      >
+                          Арт стили
+                      </NavLink>
+                  </li>
+                  <li>
+                      <NavLink
+                          style={{ textDecoration: "none", color: "white" }}
+                          to="/likeness-portrait"
+                          onClick={handleCloseNavbar}
+                      >
+                          Портрет в образе
+                      </NavLink>
+                  </li>
+                  <li>
+                      <NavLink
+                          style={{ textDecoration: "none", color: "white" }}
+                          to="/reviews"
+                          onClick={handleCloseNavbar}
+                      >
+                          Отзывы
+                      </NavLink>
+                  </li>
+              </ul>
+          </div>
+          <ClickAwayHandler onClick={handleCloseNavbar} onTouchStart={handleCloseNavbar}/>
+        </NavbarParent>
       )}
     </div>
   );
