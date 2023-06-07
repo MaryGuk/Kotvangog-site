0e53a17d76016187b0e72587e44edc3d8043be82
viacheslav <viacheslav.g@5ly.co>
2023-06-08 00:16:47 +0300

    remove carousel on first page on mobile

--- src/components/Pages/FrontPage1/FirstPage/index.jsx
+++ src/components/Pages/FrontPage1/FirstPage/index.jsx
@@ -128,7 +128,7 @@ const FirstPage = () => {
                 src: "https://www.dropbox.com/s/s93xw6avte7lufx/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%BE%D0%B5%20%D1%84%D0%BE%D1%82%D0%BE%204.jpg?raw=1",
                 key: "image4",
               },
-            ].map(({ src, key }) => (
+            ].slice(0, isMobile ? 1 : 4).map(({ src, key }) => (
               <Box height={600} key={key}>
                 <img src={src} key={key} loading="lazy" decoding="async" />
               </Box>
