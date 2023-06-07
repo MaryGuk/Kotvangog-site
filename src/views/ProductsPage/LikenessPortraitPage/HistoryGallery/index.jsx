2c2b54e6f013e8a33202e7009bff113979949c46
viacheslav <viacheslav.g@5ly.co>
2023-06-07 13:45:40 +0300

    add history carousel

--- src/views/ProductsPage/LikenessPortraitPage/HistoryGallery/index.jsx
+++ src/views/ProductsPage/LikenessPortraitPage/HistoryGallery/index.jsx
@@ -4,87 +4,14 @@ import { useState } from "react";
 import { Dialog } from "@mui/material";
 import HistoryGalleryItem from "./HistoryGalleryItem";
 import ImageWithLoader from "../../../../components/ImageWithLoader";
+import {historyImages} from "../../../../constants/galeries/historyImages";
 
 const HistoryGallery = () => {
   const [fullImageSrc, setFullImageSrc] = useState(null);
   return (
     <Box>
       <HistoryImagesGallery>
-        {[
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/bcwhbs8pncgpad1/1654_60%D1%8580.jpg?raw=1",
-            fullSrc:
-              "https://www.dropbox.com/s/muvrowlrzhvdg9a/1%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/kdf106tt6z2plyj/1770_30%D1%85400%20%D0%BC%D0%B0%D1%81%D0%BB%D0%BE.jpg?raw=1",
-            fullSrc:
-              "https://www.dropbox.com/s/pls8nplo3nddkyb/2%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/fboc9n7cxliycjv/1867_50%D1%8570.jpg?raw=1",
-            fullSrc:
-              "https://www.dropbox.com/s/b4otc7aidisaeyp/3%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/7nxhairbqipbztm/Women%20geem2364.jpg?raw=1",
-            fullSrc:
-              "https://www.dropbox.com/s/plb6hyv460qrsk5/4%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/8oz61zneaz34j0c/45353453.jpg?raw=1",
-            fullSrc:
-              "https://www.dropbox.com/s/whmxhghnbso9tr0/5%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/7um38jn3alo4zgh/5646_60%D1%8580.jpg?raw=1",
-            fullSrc:
-              "https://www.dropbox.com/s/8ibssqm4gi2r39e/6%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/d8zylbi6lvpdy11/5958_50%D1%8570%20%D1%86%D0%B0%D1%80%D1%8C%20%D0%B3%D0%BE%D1%82%D0%BE%D0%B2%D0%BE.jpg?raw=1",
-            fullSrc:
-              "https://www.dropbox.com/s/fuizg3zurzac7m5/7%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/aeauo89a8g52xe4/25385_30%D1%8540.jpg?raw=1",
-            fullSrc:
-              "https://www.dropbox.com/s/y6101yhohffuo5i/8%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/fxt11vl3q0jjpsc/9489416136.jpg?raw=1",
-            fullSrc:
-              "https://www.dropbox.com/s/0zor4ss7y0jiep5/9%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/2rdviezsohw40ld/00029D09FD0B0D180D18.jpg?raw=1",
-            fullSrc:
-              "https://www.dropbox.com/s/i7hbg7pio1x2o3e/10%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/mgsvnp6992zdo4r/Berran_51_copy_master.jpg?raw=1",
-
-            fullSrc:
-              "https://www.dropbox.com/s/nrtgaj66843p0hv/11%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-          {
-            previewSrc:
-              "https://www.dropbox.com/s/hohuc573b5uzakb/1545258747736.jpg?raw=1",
-            fullSrc:
-              "https://www.dropbox.com/s/lqtbxxxmimtgtx3/12%20%D0%B8%D1%81%D1%82.jpg?raw=1",
-          },
-        ].map(({ previewSrc, fullSrc }, idx) => (
+        {historyImages.map(({ previewSrc, fullSrc }, idx) => (
           <HistoryGalleryItem
             key={idx}
             previewSrc={previewSrc}
--- src/views/ProductsPage/LikenessPortraitPage/ModernImages/index.jsx
+++ src/views/ProductsPage/LikenessPortraitPage/ModernImages/index.jsx
@@ -25,81 +25,9 @@ import {isMobile} from "react-device-detect";
 import {Box} from "@mui/material";
 import PreviewPhotoDialog from "../../../../components/PreviewPhotoDialog";
 import usePreviewPhotoDialogState from "../../../../components/PreviewPhotoDialog/usePreviewPhotoDialogState";
+import {modernImages} from "../../../../constants/galeries/modernImages";
+import {historyImages} from "../../../../constants/galeries/historyImages";
 
-const modernImages = [
-  {
-    previewSrc:
-        " https://www.dropbox.com/s/1l7km1tfs1dqejf/78637387.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/7i4edocw0ch8y5n/1%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-  {
-    previewSrc:
-        "https://www.dropbox.com/s/zs03zpkrksh4z3p/1200_60%D1%8560.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/8vzgpzgwl1pfgvy/2%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-  {
-    previewSrc:
-        "https://www.dropbox.com/s/6l6l5iv37nmzxi5/1655897.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/v7aex39jw4sns2h/3%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-  {
-    previewSrc:
-        "https://www.dropbox.com/s/bs38jxj9is2bpb7/7863838.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/nfocse5n3f8r5wn/4%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-  {
-    previewSrc:
-        "https://www.dropbox.com/s/yvta5m0qic24o9m/25454.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/xtg7uqnpd6oyut1/5%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-  {
-    previewSrc:
-        "https://www.dropbox.com/s/yzuzwxlko82zjlv/6066_40%D1%8560.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/a00yuvwfr6fjw5c/6%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-  {
-    previewSrc:
-        "https://www.dropbox.com/s/2gkkxivrm53xehg/4757838.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/evbhunwxe5nl81n/7%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-  {
-    previewSrc:
-        "https://www.dropbox.com/s/sc5w6hgk2k60ox6/Rodriguez-2MasterJpeg9557JSWfile-753x1024.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/6i3f5tidhq14km3/8%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-  {
-    previewSrc:
-        "https://www.dropbox.com/s/po2d9anmj0j7ls1/5190_50%D1%85701.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/8r33qryxtf521xm/9%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-  {
-    previewSrc:
-        " https://www.dropbox.com/s/an8343715jxzuk7/563783.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/n0adj33in80mxtu/10%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-  {
-    previewSrc:
-        "   https://www.dropbox.com/s/xdrjvc3wylb6q5v/42402b882aaa5309efcde742.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/8gw5jispducmchu/11%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-  {
-    previewSrc:
-        "https://www.dropbox.com/s/flxkpyikmcp6d81/a4d4dca9a7e79172ece7be5.jpg?raw=1",
-    fullSrc:
-        "https://www.dropbox.com/s/7aw7yemyybpt1m0/12%20%D1%81%D0%BE%D0%B2.jpg?raw=1",
-  },
-];
 
 const ModernImages = () => {
   const {
@@ -142,7 +70,15 @@ const ModernImages = () => {
           </ModernImagesDescription>
         </ModernImagesContent>
 
-        <HistoryGallery />
+        {isMobile ? (
+          <Box pl="30px" pr="30px">
+            <MobileCarousel
+              imageList={historyImages.map(({ previewSrc}) => previewSrc)}
+              columnCount={2}
+              rowCount={3}
+              onImageClick={(idx) => setFullImageSrcData({ gallery: historyImages, idx })}
+            />
+          </Box>) : (<HistoryGallery/>)}
 
         <HistoryImagesCollage>
           <HistoryImagesCollageItems>
