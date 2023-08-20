a9f29f010d8a95590e3b6b96650b5aa260cd9ebc
viacheslav <viacheslav.g@5ly.co>
2023-08-21 02:13:58 +0300

    fix oil page

--- src/views/ProductsPage/OilPortraitPage/PortraitFrame/index.jsx
+++ src/views/ProductsPage/OilPortraitPage/PortraitFrame/index.jsx
@@ -25,28 +25,28 @@ const PortraitFrame = () => {
           >
             {[
               {
-                src: "https://www.dropbox.com/s/o5xma1675d95p66/%D0%BF%D0%B5%D1%80%D0%B2%D0%B0%D1%8F.png?raw=1",
+                src: "https://www.dropbox.com/scl/fi/f673keqtotyfv7hgign8b/750_33.png?rlkey=8irotpv2mmmv2aocmv0ns8wod&dl=0&raw=1",
                 key: "image1",
               },
               {
-                src: "https://www.dropbox.com/s/1nowladji8dny59/%D0%B2%D1%82%D0%BE%D1%80%D0%B0%D1%8F.png?raw=1",
+                src: "https://www.dropbox.com/scl/fi/bgsnbwsbjhe93ogn4ufmi/750_34.png?rlkey=wollkzx8euj34abkdnq1b9oxq&dl=0&raw=1",
                 key: "image2",
               },
               {
-                src: "https://www.dropbox.com/s/szq1sa4mvw751rq/%D1%82%D1%80%D0%B5%D1%82%D1%8C%D1%8F.png?raw=1",
+                src: "https://www.dropbox.com/scl/fi/j0lsbrfj5cfwwfs5aqsse/750_24.png?rlkey=vngesirk7t99zjlml0hmf68uu&dl=0&raw=1",
                 key: "image3",
               },
               {
-                src: "https://www.dropbox.com/s/mqt566qw6cibxa1/%D1%87%D0%B5%D1%82%D0%B2%D1%91%D1%80%D1%82%D0%B0%D1%8F.png?raw=1",
+                src: "https://www.dropbox.com/scl/fi/wta2c5lidkr0jeg870ac3/750_14.png?rlkey=s4ymimoioo1n9b6ev1ihcuul0&dl=0&raw=1",
                 key: "image4",
               },
               {
-                src: "https://www.dropbox.com/s/wqxs1gc34oog4lf/%D0%BF%D1%8F%D1%82%D0%B0%D1%8F.png?raw=1",
+                src: "https://www.dropbox.com/scl/fi/vti8x3kbvjh994gk0i6lc/750_3.png?rlkey=0ta1z5qn4mincgj9m803n83wg&dl=0&raw=1",
                 key: "image5",
               },
             ].map(({ src, key }) => (
-              <Box height={370} key={key}>
-                <img style={{ width: "100%" }} src={src} key={key} />
+              <Box height={370} key={key} display="flex" justifyContent="center">
+                <img height="100%" alt={key} src={src} key={key} />
               </Box>
             ))}
           </Carousel>
