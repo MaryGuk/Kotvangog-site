646d6841649c232bb708377f1bedd2430b7b6322
viacheslav <viacheslav.g@5ly.co>
2023-11-11 15:51:17 +0300

    add freeze on click

--- src/components/Autumn2023Update/MainSlide/MainSlideImage.jsx
+++ src/components/Autumn2023Update/MainSlide/MainSlideImage.jsx
@@ -3,17 +3,22 @@ import {mainSlideFramesGallery} from "../../../constants/galeries/mainPageImages
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import {Box} from "@mui/system";
+import {useState} from "react";
 
 const MainSlideImages = () => {
+    const [autoPlay, setAutoPlay] = useState(true);
+
     return <MainSlideImageCarouselWrapper>
-      <Box height="120vw" width="100vw" position="absolute">
+      <Box height="120vw" width="100vw" position="absolute" onTouchStart={() => setAutoPlay(false)} onTouchEnd={() => setAutoPlay(true)}>
         <Carousel
-          autoPlay
+          autoPlay={autoPlay}
           showIndicators={false}
           showThumbs={false}
           showArrows={false}
           showStatus={false}
           infiniteLoop
+          stopOnHover
+          width="120vw"
         >
           {
             mainSlideFramesGallery.map((src, idx) =>
