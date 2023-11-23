dee65d55c3a4c9684219e182b276844697b91b7a
Viacheslav Guk <33941934+slavaguk2000@users.noreply.github.com>
2023-11-24 00:23:43 +0300

    change master titles (#70)
    Co-authored-by: viacheslav <viacheslav.g@5ly.co>

--- src/components/Pages/ProcessPage/MasterName/master-name.jsx
+++ src/components/Pages/ProcessPage/MasterName/master-name.jsx
@@ -1,24 +1,21 @@
-import "./master-name.css";
+import {MasterNameWrapper} from "../styled";
+import BrashTitle from "../../../Autumn2023Update/BrashTitle";
+import {Typography} from "@mui/material";
+import Brash8 from "../../../../images/brashes/brash8.svg";
+import Brash9 from "../../../../images/brashes/brash9.svg";
+
 
 const MasterName = () => {
   return (
-    <div className="master-name__title-wrapper">
-      <div className="master-name__title-first">
-        <div className="master-name-page__title-background">
-          <img
-            className="master-name__title-background-image"
-            src="/images/background/Master name.png"
-          />
-        </div>
+    <MasterNameWrapper>
+      <BrashTitle brashSrc={Brash8} brashWidth="12rem" translateY={-20} translateX={-5}>
+        <Typography variant="h4">Любовь Ненадовец</Typography>
+      </BrashTitle>
 
-        <div className="master-name__container">
-          <div className="master-name__title">Любовь Ненадовец</div>
-          <div className="master-name__title2">
-            Художник мастерской KOTVANGOG
-          </div>
-        </div>
-      </div>
-    </div>
+      <BrashTitle brashSrc={Brash9} brashWidth="24rem" translateY={-10} translateX={2}>
+        <Typography variant="h4">Художник мастерской KOTVANGOG</Typography>
+      </BrashTitle>
+    </MasterNameWrapper>
   );
 };
 
