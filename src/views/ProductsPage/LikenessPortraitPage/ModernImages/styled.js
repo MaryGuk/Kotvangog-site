9b105133ce74e66e6cacdd50c18a09bee14c2658
Viacheslav Guk <33941934+slavaguk2000@users.noreply.github.com>
2024-04-02 14:25:50 +0200

    48-change_likeness_page_first_block (#83)
    Co-authored-by: viacheslav <viacheslav.g@5ly.co>

--- src/views/ProductsPage/LikenessPortraitPage/ModernImages/styled.js
+++ src/views/ProductsPage/LikenessPortraitPage/ModernImages/styled.js
@@ -5,6 +5,7 @@ export const ModernImagesWrapper = styled(Box)`
   display: flex;
   justify-content: center;
   width: 100%;
+  padding: 30px 0 0;
 
   @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (orientation: portrait) {
     background-color: ${({ theme }) => theme.palette.background.default};
@@ -25,7 +26,7 @@ export const ModernImagesContent = styled(Box)`
 
 export const ModernImagesDescription = styled(Box)`
   font-size: 17px;
-  font-family: 'Palatino Linotype';
+  font-family: 'Palatino Linotype', sans-serif;
   font-weight: 500;
   text-align: center;
   color: #555555;
@@ -50,7 +51,7 @@ export const HistoryImagesCollageItems = styled(Box)`
   margin-bottom: 80px;
 
   @media (max-width: 950px) {
-    display: 'flex';
+    display: flex;
     justify-content: center;
     flex-direction: column;
     align-items: center;
@@ -80,7 +81,7 @@ export const HistoryImagesCollageText = styled(Box)`
 `;
 export const HistoryImagesCollageText1 = styled(Box)`
   font-size: 24px;
-  font-family: 'Palatino Linotype';
+  font-family: 'Palatino Linotype', sans-serif;
   font-weight: 700;
   max-width: 310px;
   color: #555555;
@@ -89,7 +90,7 @@ export const HistoryImagesCollageText1 = styled(Box)`
 `;
 export const HistoryImagesCollageText2 = styled(Box)`
   font-size: 24px;
-  font-family: 'Palatino Linotype';
+  font-family: 'Palatino Linotype', sans-serif;
   font-weight: 400;
   color: #555555;
   max-width: 360px;
@@ -113,7 +114,7 @@ export const MobileLikenessTextOne = styled(Box)`
   display: none;
 
   @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (orientation: portrait) {
-    font-family: 'Century Gothic';
+    font-family: 'Century Gothic', sans-serif;
     font-weight: normal;
     font-size: 13px;
     text-align: center;
@@ -134,7 +135,7 @@ export const MobileLikenessTextTwo = styled(Box)`
   display: none;
 
   @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (orientation: portrait) {
-    font-family: 'Century Gothic';
+    font-family: 'Century Gothic', sans-serif;
     font-weight: bold;
     font-size: 13px;
     text-align: center;
@@ -157,7 +158,7 @@ export const MobileLikenessTextThree = styled(Box)`
   display: none;
 
   @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (orientation: portrait) {
-    font-family: 'Century Gothic';
+    font-family: 'Century Gothic', sans-serif;
     font-weight: normal;
     font-size: 13px;
     text-align: center;
